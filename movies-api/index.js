c5b1b3f02ca969a7523c60404f507a9e721c20ae
Noel Leahy (20085216) <20085216@wit.ie>
2021-11-15 12:56:52 +0000

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,9 +7,11 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
-
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+
+
